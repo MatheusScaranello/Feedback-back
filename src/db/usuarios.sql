cb261d948b6d0db23c002705798799c656c02374
MatheusScaranello <matheusscaranello.564@gmail.com>
2024-09-16 08:04:47 -0300

    Refatorar operações CRUD de usuários e corrigir inserção de dados

--- src/db/usuarios.sql
+++ src/db/usuarios.sql
@@ -9,4 +9,24 @@ CREATE TABLE usuario (
 );
 
 --Incert exemplo
-INSERT INTO usuario (local, nota, observacao, data) VALUES ('São Paulo', 5, 'Muito bom', '2021-01-01');
+INSERT INTO usuario (local, nota, observacao, data) VALUES ('Sao Paulo', 5, 'Muito bom', '2021-01-01');
+
+CREATE OR REPLACE FUNCTION gerar_usuarios_aleatorios(num_usuarios INT) RETURNS VOID AS $$
+DECLARE
+    i INT;
+    locais TEXT[] := ARRAY['Sao Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Curitiba', 'Porto Alegre'];
+    observacoes TEXT[] := ARRAY['Excelente servico!', 'A melhorar', 'Muito bom', 'Satisfeito', 'otima experiencia'];
+BEGIN
+    FOR i IN 1..num_usuarios LOOP
+        INSERT INTO usuario (local, nota, observacao, data)
+        VALUES (
+            locais[ceil(random() * array_length(locais, 1))], -- Seleciona um local aleatório
+            (random() * 10)::INT, -- Gera uma nota aleatória entre 0 e 10
+            observacoes[ceil(random() * array_length(observacoes, 1))], -- Seleciona uma observação aleatória
+            CURRENT_DATE - (random() * 365)::INT -- Gera uma data aleatória dentro do último ano
+        );
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT gerar_usuarios_aleatorios(100);
